f2e80eb6ad60bfa8fb007a7caefc7d31bafdd8da
numangit <abdulnumanjob@gmail.com>
2022-10-27 04:07:26 +0600

    footer social icons modified

--- src/components/BottomFooter/BottomFooter.js
+++ src/components/BottomFooter/BottomFooter.js
@@ -9,7 +9,7 @@ const BottomFooter = () => {
         <div className="bg-dark">
             <div className="container text-center text-white d-lg-flex py-5 border-top">
                 <div className="mx-auto">
-                    <ul className="nav col-md-4">
+                    <ul className="nav col-md-4 ">
                         <li className="footer-option nav-item"><Link to="/" className="nav-link px-2 text-muted">Home</Link></li>
                         <li className="footer-option nav-item"><Link to="courses" className="nav-link px-2 text-muted">Courses</Link></li>
                         <li className="footer-option nav-item"><Link to="faq" className="nav-link px-2 text-muted">FAQ</Link></li>
@@ -27,12 +27,12 @@ const BottomFooter = () => {
                 <hr className="my-1 d-lg-none" />
                 <div className="vr d-none d-lg-block col-lg-1 mx-auto"></div>
                 <div className="mx-auto p-2 my-auto">
-                    <Link className="navbar-brand" to="/"><img className="w-25" src={logo} alt="" /><span className="fw-bolder fs-3"> Rise<span className="logo-color">.</span>io</span></Link>
+                    <Link className="navbar-brand d-none d-lg-block" to="/"><img className="w-25" src={logo} alt="" /><span className="fw-bolder fs-3"> Rise<span className="logo-color">.</span>io</span></Link>
                     <p className="my-2 text-muted mx-auto">Follow us on:</p>
                     <div>
-                        <Link className="mx-2 text-muted" to="/"><BsTwitter /></Link>
-                        <Link className="mx-2 text-muted" to="/"><BsGithub /></Link>
-                        <Link className="mx-2 text-muted" to="/"><BsInstagram /></Link>
+                        <Link className="mx-2 text-muted fs-3" to="/"><BsTwitter /></Link>
+                        <Link className="mx-2 text-muted fs-3" to="/"><BsGithub /></Link>
+                        <Link className="mx-2 text-muted fs-3" to="/"><BsInstagram /></Link>
                     </div>
                     <p className="my-2 text-muted mx-auto d-lg-none">Copyright © 2017–2022 Rise.io</p>
                 </div>

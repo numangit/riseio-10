9310bdd7f8f0d33457da6067b92096a39a98833f
numangit <abdulnumanjob@gmail.com>
2023-05-31 19:59:54 +0600

    our app section created and added to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import Header from './Header/Header';
 import Instructors from './Instructors/Instructors';
+import OurApp from './OurApp/OurApp';
 
 const Home = () => {
     return (
         <div className="my-5 py-5">
             <Header></Header>
             <Instructors></Instructors>
+            <OurApp></OurApp>
         </div>
     );
 };

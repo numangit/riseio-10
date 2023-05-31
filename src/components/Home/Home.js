42d7c355ac72c330d575ec0c3fd878c075a321af
numangit <abdulnumanjob@gmail.com>
2023-05-31 21:51:58 +0600

    student section completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import Header from './Header/Header';
 import Instructors from './Instructors/Instructors';
 import OurApp from './OurApp/OurApp';
+import Success from './Success/Success';
 import TrustedCompany from './TrustedCompany/TrustedCompany';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div className="my-5 py-5">
             <Header></Header>
             <Instructors></Instructors>
+            <Success></Success>
             <OurApp></OurApp>
             <TrustedCompany></TrustedCompany>
         </div>
--- src/components/Home/Success/Success.js
+++ src/components/Home/Success/Success.js
@@ -1,18 +1,18 @@
 import student1 from '../../../assets/student-1.png';
 import student2 from '../../../assets/student-3.png';
 import student3 from '../../../assets/student-4.png';
+import student4 from '../../../assets/profile-picture-1.jpg';
 
 const Success = () => {
   return (
     <section id="success" class="container text-sm-start my-3 ">
-      <h1 class="fw-semibold display-6 my-lg-4">Meet Our Successfull
-        Students</h1>
-      <p class="text-muted my-2">Over the past 10 years, we had many Successfull students
+      <h1 class="fw-semibold display-6 my-lg-4 text-white text-center">Meet Our Successful Students</h1>
+      <p class="my-2 text-white text-center">Over the past 10 years, we had many Successfull students
         <br />and here are some of ur top brilliant students.
       </p>
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 my-2 my-lg-4">
         <div class="col">
-          <div class="card">
+          <div class="card bg-dark text-white">
             <img src={student1} class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title fw-bold">Awlad Hossaine</h5>
@@ -21,7 +21,7 @@ const Success = () => {
           </div>
         </div>
         <div class="col">
-          <div class="card">
+          <div class="card bg-dark text-white">
             <img src={student2} class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title fw-bold">Jannatul Islam</h5>
@@ -30,7 +30,7 @@ const Success = () => {
           </div>
         </div>
         <div class="col">
-          <div class="card">
+          <div class="card bg-dark text-white">
             <img src={student3} class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title fw-bold">Nishi Akhter</h5>
@@ -38,9 +38,15 @@ const Success = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div class='d-flex justify-content-center'>
-        <button class="btn btn-info text-white mt-3 mt-md-5 d-none d-sm-flex  ">View All</button>
+        <div class="col">
+          <div class="card bg-dark text-white">
+            <img src={student4} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title fw-bold">Nick Paul</h5>
+              <small class="card-text">React Developer</small>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
